23ca482822bcfca33910e7922f1854231922fdcd
tkvnocode <87977489+tkvnocode@users.noreply.github.com>
2024-08-31 15:49:13 +0200

    Initial Commit

---
+++ datago-appstreaming-example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  datago-appstreaming-example
+//
+//  Created by macbook on 31.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ datago-appstreaming-example/datago_appstreaming_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  datago_appstreaming_exampleApp.swift
+//  datago-appstreaming-example
+//
+//  Created by macbook on 31.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct datago_appstreaming_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
